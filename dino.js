e0626554031a269f9b6652aae6f5ecb4eadeff1f
Mushi0928 <38587322+Mushi0928@users.noreply.github.com>
2020-04-15 02:27:33 +0800

    1.1 basics

--- dino.js
+++ dino.js
@@ -9,6 +9,8 @@ var spacePressed = false;
 const G = 0.1;
 const heightLimit = 150;
 
+var isPlaying = true;
+
 //gameObjects
 class SceneObj{
     constructor(x,y,width,height,color){
@@ -69,11 +71,108 @@ class Player{
     }
 
 }
+//obstacle system
+class obstacleHandler{
+     constructor(baseDelay,range){
+        this.baseDelay = baseDelay;
+        this.range = range;
+        this.tick = 0;
+        this.obstacleList = [];
+        this.indexToDelete = [];
+        this.next = this.nextDelay();
+     }
+     nextDelay(){
+         var x = this.baseDelay + Math.floor(Math.random()*this.range);
+         return x;
+     }
+     createObstacle(){
+         var rand = Math.floor(Math.random()*3);
+         var obs;
+         switch (rand){
+            case 0:
+                obs = new obstacle(50,100,"blue",5);
+                this.obstacleList.push(obs);
+                //console.log("a");
+                break;
+            case 1:
+                obs = new obstacle(50,100,"green",6);
+                this.obstacleList.push(obs);
+                //console.log("b");
+                break;
+            case 2:
+                obs = new obstacle(50,100,"yellow",7);
+                this.obstacleList.push(obs);
+                //console.log("c");
+                break;
+        }    
+     }
+     update(){
+         this.tick += 1;
+         if(this.tick >= this.next){
+             this.tick = 0;
+             this.next = this.nextDelay();
+             this.createObstacle();
+         }
+         var nextList = [];
+         this.obstacleList.forEach(function(item,index,array){
+            item.update();
+            if(!item.outOfBound){
+                nextList.push(item);
+            }
+        });
+        this.obstacleList = nextList.slice();
+     }
+     render(){
+        this.obstacleList.forEach(function(item,index,array){
+            item.render();
+        });
+     }
+}
+class obstacle{
+    constructor(width,height,color,velocity){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.velocity = velocity;
+        this.x = canvas.width;
+        this.y = 300 - this.height;
+        this.outOfBound = false;
+    }
+    render(){
+        ctx.beginPath();
+        ctx.rect(this.x,this.y,this.width,this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+        //console.log("rect rendering")  
+    }
+    update(){
+        if(rectCollision(player.x,player.y,player.width,player.height,
+                         this.x,this.y,this.width,this.height)){
+                         console.log("collided");
+                         isPlaying = false;    
+                         }else{
+                             console.log("not collided");
+                         }
+        this.x -= this.velocity;
+        if(this.x < 0 - this.width){
+            this.outOfBound = true;
+        }
+    }
+}
+//collision detector
+function rectCollision(x1,y1,w1,h1,x2,y2,w2,h2){
+    if(x1+w1>x2 && x1<x2+w2 && y1+h1>y2 && y1<y2+h2){
+        return true;
+    }else{
+        return false;
+    }
+}
 //keyHandlers
 function keyDownHandler(e){
     if(e.key == ' '){
         spacePressed = true;
-        console.log('幹');
+        //console.log('幹');
     }
 }
 function keyUpHandler(e){
@@ -85,17 +184,23 @@ function keyUpHandler(e){
 function init(){
     ground = new SceneObj(0,300,canvas.width,canvas.height-200,'gray');
     player = new Player(80,80,'red');
+    obsHandler = new obstacleHandler(200,50);
     }
 function update(){
-    //update
-    player.update();
-    //render
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ground.render();
-    player.render();
- 
-    
-
+    if(isPlaying){
+        //update
+        player.update();
+        obsHandler.update();
+        //render
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ground.render();
+        player.render();
+        obsHandler.render();
+    }else{
+        var image = new Image(400,150);
+        image.src = 'https://i.imgur.com/hbjq8Ii.png';
+        ctx.drawImage(image,200,0,image.width,image.height);
+    }
 }
 //execute
 init();
